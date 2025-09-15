6f1e9844cf8ab870b15e21a14931c7da523af58b
Thomas Eses <t.eses@online.de>
2025-09-15 14:39:13 +0200

    lambda funktionen

---
+++ aufgaben/lambda.py
@@ -0,0 +1,18 @@
+
+
+# Letzte Ziffer einer Zahl
+# 1234 -> 4
+# 9876 -> 6
+
+
+# Maximum von zwei zahlen mit Lambda
+
+
+# Palindrom-Prüfung (Bonus)
+# Ein Palindrom ist ein Wort welches vorwärts und rückwärts das selbe ergibt
+# otto -> True
+# anna -> True
+# maoam -> True
+# reliefpfeiler -> True
+# Thomas -> False
+

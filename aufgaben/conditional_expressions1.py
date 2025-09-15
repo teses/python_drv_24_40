f70d1edbee1dbf450ad628cd82c0695f7c941d34
Thomas Eses <t.eses@online.de>
2025-09-15 13:36:22 +0200

    Conditional Expression Aufgaben

---
+++ aufgaben/conditional_expressions1.py
@@ -0,0 +1,57 @@
+"""
+Aufgabe : Alter prüfen
+Schreibe einen bedingten Ausdruck, der "Volljährig" ausgibt, wenn alter >= 18 ist, sonst "Minderjährig".
+"""
+alter = 18
+
+
+"""
+Aufgabe: Minimum zweier Zahlen
+Gebe das kleinere von zwei Zahlen a und b mit einem bedingten Ausdruck aus.
+"""
+a = 12
+b = 7
+
+"""
+Aufgabe : Note anhand von Punktzahl
+Verwende einen bedingten Ausdruck, um Noten zu vergeben:
+>= 90: "A"
+>= 80: "B"
+>= 70: "C"
+Sonst: "D"
+"""
+
+
+"""
+Aufgabe: Gerade oder ungerade
+Gebe "Gerade" aus, wenn eine Zahl gerade ist, sonst "Ungerade".
+"""
+
+
+"""
+Aufgabe: Absoluter Wert (Betrag)   
+Gebe den absoluten Wert einer Zahl x mit einem bedingten Ausdruck aus.
+"""
+x = 15 # 15
+x = -15 # 15
+
+"""
+Aufgabe: Alterskategorie
+
+Gebe die Alterskategorie basierend auf alter aus:
+< 13: "Kind"
+13-19: "Teenager"
+20-59: "Erwachsener"
+>= 60: "Senior"
+
+"""
+
+"""
+    Aufgabe: Wetterempfehlung
+
+    Gebe eine Empfehlung basierend auf Temperatur (temp) und Regen (regnet) aus:
+    temp >= 25 und not regnet: "Gehe schwimmen"
+    temp >= 20 und not regnet: "Gehe spazieren"
+    regnet: "Bleib drinnen"
+    Sonst: "Zieh dich warm an"
+"""

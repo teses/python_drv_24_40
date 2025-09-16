b399b102582dbb54047304f9f10ee7017243fc9c
Thomas Eses <t.eses@online.de>
2025-09-16 11:41:35 +0200

    update

---
+++ aufgaben/sort_2.py
@@ -0,0 +1,19 @@
+"""
+
+Teilaufgaben
+
+Sortiere die Liste der Bücher nach Erscheinungsjahr aufsteigend.
+Sortiere die Liste der Bücher nach der Länge des Titels.
+Sortiere die Liste der Bücher nach Autor alphabetisch, und wenn der Autor gleich ist, dann nach Erscheinungsjahr absteigend.
+
+Herausforderung
+Sortiere die Liste der Bücher nach Erscheinungsjahr aufsteigend und nach Author absteigend
+
+"""
+buecher = [
+    {"titel": "Der Prozess", "autor": "Franz Kafka", "jahr": 1925},
+    {"titel": "Faust", "autor": "Johann Wolfgang von Goethe", "jahr": 1808},
+    {"titel": "Die Verwandlung", "autor": "Franz Kafka", "jahr": 1915},
+    {"titel": "Die Physiker", "autor": "Friedrich Dürrenmatt", "jahr": 1962},
+    {"titel": "Homo Faber", "autor": "Max Frisch", "jahr": 1957},
+]

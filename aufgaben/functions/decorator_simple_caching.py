c8da3b769213350c34c1225700af28a3c65f6d65
Thomas Eses <t.eses@online.de>
2025-09-18 10:41:27 +0200

    update

--- aufgaben/functions/decorator_simple_caching.py
+++ aufgaben/functions/decorator_simple_caching.py
@@ -6,6 +6,8 @@ Tipp;
 - im cache muss man die daten wiederfinden
 - mit funktionsnamen und den parametern einen eindeutigen schlüssel bilden
 
+
+Diese Technik nennt man Memoisation
 """
 
 
@@ -18,17 +20,49 @@ def simple_cache(func):
     cache = {}
 
     def wrapper(*args, **kwargs):
-        #print(f"Vor der Funktion: {func.__name__}")
-        ret = func(*args, **kwargs)
-        #print(f"Nach der Funktion: {func.__name__}")
-        return ret
+
+        # Schritt 1: Schlüssel erstellen
+        key = str(hash(str(func.__name__)+str((args, tuple(sorted(kwargs.items()))))))
+
+        # Schritt 2: Prüfen, ob Ergebnis im Cache ist
+        if key in cache:
+            return cache[key]
+
+        # Schritt 3: Ergebnis neu berechnen
+        else :
+            # ursprüngliche Funktion ausführen
+            ret = func(*args, **kwargs)
+            # Ergebnis im Cache speichern
+            cache[key] = ret
+            return ret
+
 
     return wrapper
 
+@simple_cache
+def get_userdata(id):
+    time.sleep(2) # simulierung einer langsammen abfrage
+    return {"id":id, "name":"user_"+str(id), "age": 20}
+
+
+
+@simple_cache
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
 @simple_cache
 def addiere(x, y):
     print("addiere aufgerufen")
+    time.sleep(2)
+    return x + y
+
+@simple_cache
+def subtrahiere(x, y):
+    print("subtrahiere aufgerufen")
     time.sleep(2)
     return x + y
 
@@ -40,15 +74,35 @@ def quadriere(x):
 
 
 
-
-print(addiere(5, 3))  # dauert 2 sekunden
+"""
+print("dauert 2 sekunden", addiere(x=5, y=3))  # dauert 2 sekunden
 print("-"*10)
-print(addiere(5, 3))  # kommt sofort
+print("dauert 2 sekunden", subtrahiere(x=5, y=3))  # dauert 2 sekunden
 print("-"*10)
-print(addiere(5, 3))  # kommt sofort
+print("kommt sofort", addiere(y=3, x=5))  # kommt sofort
 print("-"*10)
-print(addiere(10, 5))  # dauert 2 sekunden
+print("dauert 2 sekunden", addiere(5, y=3))  # dauert 2 sekunden
 print("-"*10)
-print(addiere(10, 5)) # kommt sofort
-
+print("dauert 2 sekunden", addiere(5, 3))  # kommt sofort
+print("-"*10)
+print("kommt sofort", addiere(5, 3))  # kommt sofort
+print("-"*10)
+print("dauert 2 sekunden", addiere(10, 5))  # dauert 2 sekunden
+print("-"*10)
+print("kommt sofort", addiere(10, 5)) # kommt sofort
+"""
 #print(quadriere(3))
+
+#print(fibonacci(36))
+#print(fibonacci(36))
+#print(fibonacci(37))
+#print(fibonacci(400))
+
+
+print(get_userdata(1))
+print(get_userdata(1))
+print(get_userdata(1))
+
+print(get_userdata(2))
+print(get_userdata(2))
+

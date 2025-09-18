d9af8eb23c54292f18a9549e3751a9b21a1498db
Thomas Eses <t.eses@online.de>
2025-09-18 14:03:22 +0200

    update

--- aufgaben/map_filter_reduce/map_filter_reduce_personendaten.py
+++ aufgaben/map_filter_reduce/map_filter_reduce_personendaten.py
@@ -9,6 +9,8 @@ Aufgabenstellung
 - Berechne das Gesamtgehalt aller Berliner Personen.
 
 """
+from functools import reduce
+
 personen = [
     {"name": "Anna", "alter": 23, "stadt": "Berlin", "gehalt": 3200},
     {"name": "Ben", "alter": 31, "stadt": "München", "gehalt": 4100},
@@ -16,5 +18,14 @@ personen = [
     {"name": "David", "alter": 45, "stadt": "Berlin", "gehalt": 5200},
     {"name": "Eva", "alter": 29, "stadt": "München", "gehalt": 3800},
 ]
+# berliner
+berliner_personen = list(filter(lambda person: person["stadt"] == "Berlin", personen))
+print(berliner_personen)
 
+# namen
+berliner_personen_namen = list(map(lambda person: person["name"], berliner_personen))
+print(berliner_personen_namen)
 
+#
+berliner_personen_gehalt_summe = reduce(lambda s, o: s + o["gehalt"], berliner_personen, 0)
+print(berliner_personen_gehalt_summe)

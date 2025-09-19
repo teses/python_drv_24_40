ad7d803a680582bee6d15301615712967fdb9845
Thomas Eses <t.eses@online.de>
2025-09-19 15:56:38 +0200

    update

---
+++ module/matplotlib/matplotlib_basics.py
@@ -0,0 +1,10 @@
+"""
+matplotlib ist eine beliebte Bibliothek in Python zur Datenvisualisierung.
+Sie wird vor allem genutzt, um Diagramme, Plots und Grafiken zu erstellen.
+
+ pip install matplotlib
+
+
+"""
+import matplotlib
+print(matplotlib.__version__)

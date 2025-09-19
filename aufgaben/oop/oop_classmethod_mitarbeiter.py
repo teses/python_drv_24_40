9f3b4b8f15ec9c6bf0e45745ddfa3f7ecc1ed7ab
Thomas Eses <t.eses@online.de>
2025-09-19 11:44:19 +0200

    update

--- aufgaben/oop/oop_classmethod_mitarbeiter.py
+++ aufgaben/oop/oop_classmethod_mitarbeiter.py
@@ -9,12 +9,55 @@ Du sollst eine Klasse Mitarbeiter schreiben:
 - Erstelle eine Klassenmethode get_by_department(department), die alle Mitarbeiter einer bestimmten Abteilung zurückgibt.
 - Füge eine Klassenmethode count() hinzu, die die Gesamtanzahl der Mitarbeiter liefert.
 """
+class Mitarbeiter:
+
+    all_mitarbeiter = []
+
+    def __init__(self, name, department):
+        self.name = name
+        self.department = department
+        Mitarbeiter.all_mitarbeiter.append(self)
+
+    # constructor
+    @classmethod
+    def from_string(cls, string):
+        name, department = string.replace(" ", "").split(",")
+        return cls(name, department)
+
+    @classmethod
+    def get_all_mitarbeiter(cls):
+        return cls.all_mitarbeiter
+
+    @classmethod
+    def get_by_department(cls, department):
+        return [mitarbeiter for mitarbeiter in cls.all_mitarbeiter if mitarbeiter.department == department]
+
+    @classmethod
+    def get_by_name(cls, name):
+        return [mitarbeiter for mitarbeiter in cls.all_mitarbeiter if mitarbeiter.name == name]
+
+    @classmethod
+    def count(cls):
+        return len(cls.all_mitarbeiter)
+
+    def __str__(self):
+        return f"{self.name}, {self.department}"
+
 
 
 # --- Test ---
-m1 = Mitarbeiter("Anna", "IT")
-m2 = Mitarbeiter.from_string("Ben, HR")
-m3 = Mitarbeiter.from_string("Clara, IT")
+m1 = Mitarbeiter("Max", "HR")
+m2 = Mitarbeiter("Monika", "HR")
+m3 = Mitarbeiter.from_string("Moritz, IT")
+m4 = Mitarbeiter("Anna", "IT")
+m5 = Mitarbeiter("Janik", "HR")
+m6 = Mitarbeiter("Ben", "IT")
+m7 = Mitarbeiter("Clara", "Sales")
+m8 = Mitarbeiter("Jonathan", "Sales")
+m9 = Mitarbeiter("Lukas", "IT")
+m10 = Mitarbeiter("Lena", "HR")
+m11 = Mitarbeiter("Lily", "IT")
+m12 = Mitarbeiter("Gregory", "HR")
 
 
 print("Gesamtanzahl:", Mitarbeiter.count())
@@ -22,7 +65,8 @@ print("Gesamtanzahl:", Mitarbeiter.count())
 ###
 itAbteilung = Mitarbeiter.get_by_department("IT")
 for m in itAbteilung:
-    print(m.name, m.abteilung)
+    print(m.name, m.department)
 
 ##
-print("HR Abteilung", [m.name + ", " + m.abteilung for m in Mitarbeiter.get_by_department("HR")])
+print("IT Abteilung", [m.__str__() for m in Mitarbeiter.get_by_department("IT")])
+print("HR Abteilung", [m.name + ", " + m.department for m in Mitarbeiter.get_by_department("HR")])
